225fab64951861cccfee24e11d4916787188d619
akucherenko <ku4er.prg@gmail.com>
2013-01-01 14:17:39 +0200

    Initial commit

---
+++ radio.php
@@ -0,0 +1,41 @@
+<?php
+
+isset($_GET['cmd']) || $_GET['cmd'] = 'list';
+$response = array();
+$stationList = array();
+$stationList['Jazz'] = 'http://streaming208.radionomy.com:80/A-JAZZ-FM-WEB';
+$stationList['FoxNews'] = 'http://fnradio-shoutcast-32.ng.akacast.akamaistream.net/7/115/13873/v1/auth.akacast.akamaistream.net/fnradio-shoutcast-32';
+
+session_start();
+isset($_SESSION['volume']) || $_SESSION['volume'] = 4;
+
+switch ($_GET['cmd'])
+{
+    case 'list':
+        $response['list'] = $stationList;
+        break;
+    case 'play':
+        $_SESSION['play'] = $_GET['station'];
+        $response['station'] = $stationList[$_GET['station']];
+        break;
+    case 'stop':
+        if(isset($_SESSION['play'])) unset($_SESSION['play']);
+        $response['result'] = 1;
+        break;
+    case 'status':
+        if (isset($_SESSION['play']))
+        {
+            $response['status'] = 'play';
+            $response['station'] = $_SESSION['play'];
+        }
+        else
+        {
+            $response['status'] = 'stop';
+        }
+    case 'volume':
+        isset($_GET['volume']) && $_SESSION['volume'] = $_GET['volume'];
+        $response['level'] = $_SESSION['volume'];
+    default:
+}
+header('Content-type:application/json');
+echo json_encode(array('response' => $response));
